4a0faa2d5ac336e913088c84f44d362a3ec4689c
chenjianhua <1778868778@qq.com>
2024-10-05 15:04:21 +0800

    更新Comments.vue 组件

--- .vitepress/theme/components/Comments.vue
+++ .vitepress/theme/components/Comments.vue
@@ -7,8 +7,7 @@ import Gitalk from "gitalk";
 import { onContentUpdated, useRouter } from "vitepress";
 
 // const { route, go } = useRouter();
-function deleteChild() {
-  const element = document.querySelector("#gitalk-container");
+function deleteChild(element: HTMLDivElement | null) {
   let child = element?.lastElementChild;
   while (child) {
     element?.removeChild(child);
@@ -17,18 +16,23 @@ function deleteChild() {
 }
 onContentUpdated(() => {
   // reset gittalk element for update
-  deleteChild();
+  const element = document.querySelector("#gitalk-container");
+  if (!element) {
+    return;
+  }
+  deleteChild(element);
   const gitalk = new Gitalk({
     clientID: "21ba97714c82cd5e7db9",
-    clientSecret: "48f2ba380ac4ea8d698915b319f0d2e618c8299a",
+    clientSecret: "9c163baace2fae9fd46fd4f3f6512bf1ea27fcb6",
     repo: "vitepress-blog-comments",
     owner: "jianhua1996",
     admin: ["jianhua1996"],
     id: location.pathname.substring(0, 50), // Ensure uniqueness and length less than 50
     language: "zh-CN",
-    distractionFreeMode: false, // Facebook-like distraction free mode
+    distractionFreeMode: true, // Facebook-like distraction free mode
   });
   gitalk.render("gitalk-container");
 });
+
 </script>
 <style scoped></style>
---
+++ posts/Flutter 组件学习（二）.md
@@ -0,0 +1,11 @@
+---
+title: Flutter 组件学习（二）
+tags: 
+  - Flutter
+  - Dart
+
+
+---
+
+
+## 1. Padding 组件
