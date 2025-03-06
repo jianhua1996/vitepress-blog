4109d106ab234e8501e7121f9bf190a788a258ce
chen <1778868778@qq.com>
2025-03-06 14:06:10 +0800

    添加文章

--- .vitepress/theme/components/ImageBuilder.vue
+++ .vitepress/theme/components/ImageBuilder.vue
@@ -1,20 +1,19 @@
 <template>
-  <div
-    class="image-builder"
-    style="width: 100%; display: flex; flex-wrap: wrap"
-    :style="{
-      justifyContent:
-        source.length > 1 && size === 'small' ? 'space-between' : 'initial'
-    }"
-  >
-    <img
-      v-for="item in source"
-      :src="withBase(`/${item}`)"
-      alt="image"
-      style="object-fit: contain"
-      :style="style[size]"
-    />
-  </div>
+	<div
+		class="image-builder"
+		style="width: 100%; display: flex; flex-wrap: wrap"
+		:style="{
+			justifyContent: source.length > 1 && size === 'small' ? 'space-between' : 'initial'
+		}"
+	>
+		<img
+			v-for="item in source"
+			:src="withBase(`/${item}`)"
+			alt="image"
+			style="object-fit: contain"
+			:style="style[size]"
+		/>
+	</div>
 </template>
 
 <script lang="ts" setup>
@@ -24,24 +23,24 @@ import { withBase } from 'vitepress'
 type Size = 'large' | 'medium' | 'small'
 
 interface Props {
-  source: string[]
-  size: Size
+	source: string[]
+	size: Size
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  size: 'medium'
+	size: 'medium'
 })
 
 const style = ref({
-  large: {
-    width: '100%'
-  },
-  medium: {
-    width: '66%'
-  },
-  small: {
-    width: '33%'
-  }
+	large: {
+		width: '100%'
+	},
+	medium: {
+		width: 'calc(100% / 3 * 2)'
+	},
+	small: {
+		width: 'calc(100% / 3)'
+	}
 })
 </script>
 
